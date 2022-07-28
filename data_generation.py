e93ed04cfbc9e99b77185bf87fea4aade4449c7f
khasmamad99 <sh.khasmamad@gmail.com>
2022-07-28 18:00:00 +0200

    first working version of data generator

--- data_generation.py
+++ data_generation.py
@@ -1,7 +1,8 @@
-from typing import Callable
+from typing import Callable, List
 from functools import partial
 from pathlib import Path
 import json
+import os
 
 import open3d as o3d
 import numpy as np
@@ -143,7 +144,7 @@ def scan_object(
     coverage_thresh: float = 0.7, 
     max_iter: int = 10,
 ):
-    if not starting_view:
+    if starting_view is None:
         view = random_sphere_point(npoints=1, radius=sphere_radius).flatten()
     else:
         view = starting_view
@@ -176,9 +177,10 @@ def scan_object(
 
 
 def generate_data(
-    mesh_object: o3d.geometry.TriangleMesh,
-    gt_grid: Grid,  # temporary
+    path_to_objs: List[Path],
+    outpout_dir: Union[str, Path] = 'data/data/ShapeNetCore.v2_nbv',
     num_voxels: int = 32,
+    num_starting_views: int = 1,
     sphere_radius: float = np.sqrt(3.1),  # encompasses unit square
     coverage_thresh: float = 0.7, 
     max_iter: int = 10,
@@ -186,74 +188,91 @@ def generate_data(
     camera_intrinsic_mtx: Union[np.ndarray, o3d.camera.PinholeCameraIntrinsic] = None,
     debug: bool = False
 ):
-    """
-    Given an object O:
-    1. Create an initial partial model M
-        1. Center O at (0, 0, 0)
-        2. Create an empty 32x32x32 voxel grid M - partial model, where each voxel is initally labeled as `unseen`
-        3. Pick a random view v on the sphere encompassing O
-        4. Generate rays from this view
-        5. Label each voxel in M via tracing each ray as either empty or occupied
-    2. Until the object is fully seen, i.e., no unseen voxels
-        1. Find the next views - views to the North, East, West, and South of the current view v
-        2. For each next view
-            1. Generate rays from the view
-            2. Label each voxel in M creating M' via tracing each ray
-            3. Utility of this view = (# of unseen voxels in M) - (# of unseen voxels in M')
-        3. Pick the view with the best utility v* and dump (M, v*)
-        4. Update M, M = M'(created with rays from v*)
-    """
+    """Function for generating data.
 
-    if not gt_grid:
+    First, 3D objects are read from the paths given with `path_to_objs`. Then a ground truth voxel grid
+    is created from each of them. Then, the following algorithm is applied:
+    ```
+    Given: Ground Truth Model G
+    1. Create a list of random starting views LS on a sphere around G
+    2. For each starting view sv in LS
+        1. Init partial model P similar to G but with unseen voxels
+        2. Scan G from sv and incorporate results into P
+        3. While P is incomplete and total number of scans < max_iter
+            1. Find NBV v*  // Algorithm 2
+            2. Store the data tuple (P, v*, direction(v*), utility(v*))
+            3. Scan G from v*
+            4. Incorporate discovered occupied and empty voxels into P
+        5. If P is complete, dump the data tuples.
+
+    ```
+    """
+    nbv_fn = partial(
+        next_best_view_exhaustive,
+        utility_fn = num_discovered_occupied_voxels,
+    )   
+
+    for idx, obj_path in enumerate(path_to_objs):
+        synset_id, obj_id = obj_path.parts[-4:-2]
+        if debug:
+            print(f"\nProcessing {synset_id}/{obj_id}\t[{idx+1}/{len(path_to_objs)}]")
         # setup ground truth grid
-        grid_data, voxel_size, min_bound, max_bound = to_occupancy_grid(
-            mesh_object, input_type="mesh", do_normalization=True, num_voxels=num_voxels
-        )
+        try:
+            obj_mesh = o3d.io.read_triangle_mesh(str(obj_path))
+            grid_data, voxel_size, min_bound, max_bound = to_occupancy_grid(
+                obj_mesh, input_type="mesh", do_normalization=True, num_voxels=num_voxels
+            )   
+        except:
+            print(f"Error loading {obj_path}. Skipping")
+            continue
         gt_grid = Grid(voxel_size, num_voxels, Vec3D(min_bound), Vec3D(max_bound))
         gt_grid.data = grid_data
         if debug:
             print("Finished setting the Ground Truth Grid")
-    
-    nbv_fn = partial(
-        next_best_view_exhaustive,
-        utility_fn = num_discovered_occupied_voxels,
-    )
 
-    obj_dict = dict()
-    for idx, scan in enumerate(scan_object(
-        gt_grid, 
-        nbv_fn,
-        camera_type=camera_type,
-        camera_intrinsic_mtx=camera_intrinsic_mtx,
-        sphere_radius=sphere_radius, 
-        coverage_thresh=coverage_thresh, 
-        max_iter=max_iter
-    )):
-        best_view_dir, best_view, partial_grid_data, best_utility = scan
-        if best_view_dir:  # is None if the scanning is over
-            sample_info = {
-                "next_view_dir"     : best_view_dir,
-                "next_view_coords"  : best_view.tolist(),
-                "partial_model"     : partial_grid_data.tolist(),
-                "next_view_utility" : int(best_utility)
-            }
+        starting_views = random_sphere_point(npoints=num_starting_views, radius=sphere_radius)
+        for sview_idx, sview in enumerate(starting_views):
             if debug:
-                print(f"Received scan no {idx}")
-                for key, val in sample_info.items():
-                    if key == "partial_model":
-                        num_occupied = (np.array(val) == VoxelType.occupied).sum()
-                        print(f"\t# of occed : {num_occupied}")
-                    else:
-                        print(f"\t{key} : {val}")
-                print()
-            obj_dict[idx] = sample_info
-        else:
-            coverage = (partial_grid_data == VoxelType.occupied).sum() \
-                    / (gt_grid.data == VoxelType.occupied).sum()
-            # discard the ones with inadequate coverage
-            if coverage > coverage_thresh:
-                file_name = Path('data/data/trial/bun.json')
-                if debug:
-                    print(f"Coverage is {coverage}, writing data to {file_name}")
-                with open(file_name, 'w', encoding='utf-8') as f:
-                    json.dump(obj_dict, f, ensure_ascii=False, indent=4)
+                print(f"Scan session {sview_idx+1}/{num_starting_views} from starting view {sview}")
+            obj_dict = dict()
+            for scan_idx, scan in enumerate(scan_object(
+                gt_grid, 
+                nbv_fn,
+                starting_view=sview,
+                camera_type=camera_type,
+                camera_intrinsic_mtx=camera_intrinsic_mtx,
+                sphere_radius=sphere_radius, 
+                coverage_thresh=coverage_thresh, 
+                max_iter=max_iter
+            )):
+                best_view_dir, best_view, partial_grid_data, best_utility = scan
+                if best_view_dir:  # is None if the scanning is over
+                    sample_info = {
+                        "next_view_dir"     : best_view_dir,
+                        "next_view_coords"  : best_view.tolist(),
+                        "partial_model"     : partial_grid_data.tolist(),
+                        "next_view_utility" : int(best_utility)
+                    }
+                    if debug:
+                        print(f"\tReceived scan no {scan_idx}")
+                        for key, val in sample_info.items():
+                            if key == "partial_model":
+                                num_occupied = (np.array(val) == VoxelType.occupied).sum()
+                                print(f"\t\t# of occed : {num_occupied}")
+                            else:
+                                print(f"\t\t{key} : {val}")
+                        print()
+                    obj_dict[scan_idx] = sample_info
+                else:
+                    coverage = (partial_grid_data == VoxelType.occupied).sum() \
+                            / (gt_grid.data == VoxelType.occupied).sum()
+                    # discard the ones with inadequate coverage
+                    if coverage > coverage_thresh:
+                        file_dir_path = Path(outpout_dir, synset_id, obj_id, str(sview_idx))
+                        if debug:
+                            print(f"\tCoverage is {coverage}, writing data to {file_dir_path}")
+                        os.makedirs(file_dir_path, exist_ok=True)
+                        for scan_idx, scan_dict in obj_dict.items():
+                            file_path = Path(file_dir_path / f'{scan_idx}.json')
+                            with open(file_path, 'w', encoding='utf-8') as f:
+                                json.dump(scan_dict, f, ensure_ascii=False, indent=4)
