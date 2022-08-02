5b2ea11b996b7c1a54e0d6ba6f523fe4db1dab79
khasmamad99 <sh.khasmamad@gmail.com>
2022-08-02 16:02:20 +0200

    implement option load pickle or json

--- data/modules/shapenet_nbv.py
+++ data/modules/shapenet_nbv.py
@@ -1,19 +1,30 @@
 from pathlib import Path
 import json
+import pickle
 
 import torch
 from torch.utils.data import Dataset
 
-dir2idx = {
-    'up'   : 0,
-    'down' : 1,
-    'right': 2,
-    'left' : 3
-}
 
 class ShapeNetNBV(Dataset):
 
-    def __init__(self, data_dir_path: Path):
+    dir2idx = {
+        'up'   : 0,
+        'down' : 1,
+        'right': 2,
+        'left' : 3
+    }
+
+    idx2dir = {
+        0 : 'up',
+        1 : 'down',
+        2 : 'right',
+        3 : 'left'
+    }
+
+    def __init__(self, data_dir_path: Path, file_extension: str = 'pickle'):
+        assert file_extension in ['pickle', 'json']
+        self.file_extension = file_extension
         synset_ids = list(data_dir_path.glob('*'))
         self.path_to_files = list()
         for synset_id in synset_ids:
@@ -21,7 +32,7 @@ class ShapeNetNBV(Dataset):
             for obj_id in obj_ids:
                 scan_sequences = list(obj_id.glob('*'))
                 for scan_sequence in scan_sequences:
-                    scans = list(scan_sequence.glob('*'))
+                    scans = list(scan_sequence.glob(f'*{file_extension}'))
                     self.path_to_files += scans
 
     def __len__(self):
@@ -29,12 +40,23 @@ class ShapeNetNBV(Dataset):
 
     def __getitem__(self, idx):
         path = self.path_to_files[idx]
+        if self.file_extension == 'pickle':
+            scan = self.load_pickle(path)
+        elif self.file_extension == 'json':
+            scan = self.load_json(path)
+        partial_model = torch.Tensor(scan['partial_model']).float()
+        nbv = torch.LongTensor([self.dir2idx[scan['next_view_dir']]])
+        return {
+            'partial_model': partial_model,
+            'nbv' : nbv
+        }
+
+    def load_json(self, path):
         with open(path, 'r') as f:
             scan = json.load(f)
-            partial_model = torch.Tensor(scan['partial_model']).float()
-            nbv = torch.LongTensor([dir2idx[scan['next_view_dir']]])
-            return {
-                'partial_model': partial_model,
-                'nbv' : nbv
-            }
+        return scan
 
+    def load_pickle(self, path):
+        with open(path, 'rb') as f:
+            scan = pickle.load(f)
+        return scan
