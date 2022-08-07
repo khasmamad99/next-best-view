f5c01c1dcbd3cf9b8cd87b5bbab2504a200fc281
khasmamad99 <sh.khasmamad@gmail.com>
2022-08-07 10:05:01 +0200

    add accuracy metrics

--- networks/lit_modules/lit_voxnet.py
+++ networks/lit_modules/lit_voxnet.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 import pytorch_lightning as pl
+import torchmetrics
 
 from .. import VoxNet
 
@@ -11,6 +12,8 @@ class LitVoxNet(pl.LightningModule):
 		super().__init__()
 		self.model = VoxNet(num_classes=num_classes)
 		self.overfit = overfit
+		self.train_acc = torchmetrics.Accuracy()
+		self.val_acc = torchmetrics.Accuracy()
 
 	def forward(self, x):
 		x = self.model(x)
@@ -25,17 +28,22 @@ class LitVoxNet(pl.LightningModule):
 		return optimizer
 
 	def training_step(self, train_batch, batch_idx):
-		loss = self.step(train_batch)
+		loss = self.step(train_batch, batch_idx, self.train_acc)
 		self.log('train_loss', loss)
+		self.log('train_acc', self.train_acc, on_step=False, on_epoch=True)
 		return loss
 
 	def validation_step(self, val_batch, batch_idx):
-		loss = self.step(val_batch)
+		loss = self.step(val_batch, batch_idx, self.val_acc)
 		self.log('val_loss', loss)
+		self.log('val_acc', self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
 
-	def step(self, batch):
+
+	def step(self, batch, batch_idx, acc_metric):
 		x, y = batch
+		y = y.flatten()
 		out = self(x)
-		loss = F.cross_entropy(out, y.flatten())
+		loss = F.cross_entropy(out, y)
+		acc_metric(out, y)
 		return loss
     
