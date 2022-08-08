aaa9a388cece2586bde464fdb0a73aa98b6a864b
khasmamad99 <sh.khasmamad@gmail.com>
2022-08-08 15:52:00 +0200

    log num samples & parametrize num data loader workers

--- data/lit_modules/lit_shapenet_nbv.py
+++ data/lit_modules/lit_shapenet_nbv.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from pathlib import Path
+import logging
 
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
@@ -17,12 +18,16 @@ class LitShapeNetNBV(pl.LightningDataModule):
         num_train_objs: int = 200,
         num_val_objs: int = 25,
         num_test_objs: int = 25,
+        num_trainloader_workers: int = 1,
+        num_valloader_workers: int = 1
     ):
         super().__init__()
         self.data_dir_path = Path(data_dir_path)
         self.file_extension = file_extension
         self.batch_size = batch_size
         self.overfit = overfit
+        self.num_trainloader_workers = num_trainloader_workers
+        self.num_valloader_workers = num_valloader_workers
         if overfit:
             # load only a single object
             self.train_objs_start, self.train_objs_end = 0, 1
@@ -33,6 +38,8 @@ class LitShapeNetNBV(pl.LightningDataModule):
             self.val_objs_start, self.val_objs_end = self.train_objs_end, self.train_objs_end + num_val_objs
             self.test_objs_start, self.test_objs_end = self.val_objs_end, self.val_objs_end + num_test_objs
 
+        self.logger = logging.getLogger("pytorch_lightning")
+
 
     def setup(self, stage: Optional[str] = None):
         self.train_set = ShapeNetNBV(
@@ -41,14 +48,20 @@ class LitShapeNetNBV(pl.LightningDataModule):
             self.data_dir_path, self.file_extension, self.val_objs_start, self.val_objs_end)
         self.test_set = ShapeNetNBV(
             self.data_dir_path, self.file_extension, self.test_objs_start, self.test_objs_end)
+        
+        self.logger.info(
+            f"Num train samples: {len(self.train_set)}\n" +
+            f"Num val samples: {len(self.val_set)}\n" +
+            f"Num test samples: {len(self.test_set)}"
+        )
 
 
     def train_dataloader(self):
-        return DataLoader(self.train_set, self.batch_size, shuffle=True, num_workers=1, persistent_workers=True)
+        return DataLoader(self.train_set, self.batch_size, shuffle=True, num_workers=self.num_trainloader_workers, persistent_workers=True)
 
 
     def val_dataloader(self):
-        return DataLoader(self.val_set, self.batch_size, shuffle=False, num_workers=1, persistent_workers=True)
+        return DataLoader(self.val_set, self.batch_size, shuffle=False, num_workers=self.num_valloader_workers, persistent_workers=True)
 
     
     def test_dataloader(self):
